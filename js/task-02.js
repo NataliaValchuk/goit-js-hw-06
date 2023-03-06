62a5f1c551a6af9545f11560882e3b0bad890705
NataliaValchuk <trewax321@gmail.com>
2023-03-06 18:50:52 +0200

    added tasks

---
+++ js/task-02.js
@@ -0,0 +1,19 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+const ulEl = document.getElementById('ingredients');
+
+for (const ingredient of ingredients) {
+  const listItem = document.createElement('li'); // <li></li>
+
+  listItem.innerText = ingredient; // <li>{ingredient}</li>
+
+  ulEl.append(listItem);
+
+  console.log(listItem);
+}
---
+++ js/task-03.js
@@ -0,0 +1,32 @@
+const galleryContainer = document.querySelector('ul');
+
+const images = [
+  {
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+];
+
+const galleryItems = images
+  .map(({ url, alt }) => {
+    return `
+    <li>
+      <img
+        src="${url}"
+        alt="${alt}"
+        style="width: 100%; height: auto; object-fit: cover;"
+      >
+    </li>
+  `;
+  })
+  .join('');
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryItems);
